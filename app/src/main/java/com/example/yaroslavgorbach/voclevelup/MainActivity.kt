b082ce53c1f857c4e70415cbf817e79e89dd0b9a
YaroslavGorbach-code <yaroslavgorbach2@gmail.com>
2021-04-09 14:04:33 +0300

    Add word button and empty fragment added

--- app/src/main/java/com/example/yaroslavgorbach/voclevelup/MainActivity.kt
+++ app/src/main/java/com/example/yaroslavgorbach/voclevelup/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.FragmentTransaction
 import androidx.fragment.app.commit
 import com.example.yaroslavgorbach.voclevelup.data.Word
 import com.example.yaroslavgorbach.voclevelup.screen.WordsFragment
+import com.example.yaroslavgorbach.voclevelup.screen.addword.AddWordFragment
 import com.example.yaroslavgorbach.voclevelup.screen.word.WordFragment
 
 class MainActivity : AppCompatActivity(R.layout.activity_main), WordsFragment.Host {
@@ -21,7 +22,15 @@ class MainActivity : AppCompatActivity(R.layout.activity_main), WordsFragment.Ho
 
     override fun openWord(word: Word) {
         supportFragmentManager.commit {
-            replace(R.id.mainContainer, WordFragment.newInstance(word))
+            replace(R.id.mainContainer, WordFragment::class.java, WordFragment.argsOf(word))
+            setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            addToBackStack(null)
+        }
+    }
+
+    override fun openAddWord() {
+        supportFragmentManager.commit {
+            replace(R.id.mainContainer, AddWordFragment())
             setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
             addToBackStack(null)
         }
---
+++ app/src/main/java/com/example/yaroslavgorbach/voclevelup/screen/addword/AddWordWragment.kt
@@ -0,0 +1,7 @@
+package com.example.yaroslavgorbach.voclevelup.screen.addword
+
+import androidx.fragment.app.Fragment
+import com.example.yaroslavgorbach.voclevelup.R
+
+class AddWordFragment : Fragment(R.layout.fragment_add_word) {
+}
