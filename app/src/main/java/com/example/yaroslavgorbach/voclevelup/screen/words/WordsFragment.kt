e7962e9f651c65069d6ea18b105e94a062d6a213
YaroslavGorbach-code <yaroslavgorbach2@gmail.com>
2021-04-10 15:07:40 +0300

    Use view model delegate in words fragment

--- app/src/main/java/com/example/yaroslavgorbach/voclevelup/screen/words/WordsFragment.kt
+++ app/src/main/java/com/example/yaroslavgorbach/voclevelup/screen/words/WordsFragment.kt
@@ -3,6 +3,7 @@ package com.example.yaroslavgorbach.voclevelup.screen.words
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.example.yaroslavgorbach.voclevelup.R
 import com.example.yaroslavgorbach.voclevelup.databinding.FragmentWordsBinding
@@ -11,7 +12,7 @@ import com.example.yaroslavgorbach.voclevelup.nav
 class WordsFragment : Fragment(R.layout.fragment_words) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val vm = ViewModelProvider(this)[WordsViewModel::class.java]
+        val vm by viewModels<WordsViewModel>()
         val wordsView = WordsViewImp(
             FragmentWordsBinding.bind(view),
             nav::openWord, nav::openAddWord)
