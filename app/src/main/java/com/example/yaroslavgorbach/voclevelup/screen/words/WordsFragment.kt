a07d603468fa4e320d9e9e1a015a7cfccba6dc70
YaroslavGorbach-code <yaroslavgorbach2@gmail.com>
2021-04-10 13:51:48 +0300

    Use view binding in words fragment

--- app/src/main/java/com/example/yaroslavgorbach/voclevelup/screen/words/WordsFragment.kt
+++ app/src/main/java/com/example/yaroslavgorbach/voclevelup/screen/words/WordsFragment.kt
@@ -5,13 +5,16 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.yaroslavgorbach.voclevelup.R
+import com.example.yaroslavgorbach.voclevelup.databinding.FragmentWordsBinding
 import com.example.yaroslavgorbach.voclevelup.nav
 
 class WordsFragment : Fragment(R.layout.fragment_words) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val vm = ViewModelProvider(this)[WordsViewModel::class.java]
-        val wordsView = WordsViewImp(view, nav::openWord, nav::openAddWord)
+        val wordsView = WordsViewImp(
+            FragmentWordsBinding.bind(view),
+            nav::openWord, nav::openAddWord)
         vm.words.observe(viewLifecycleOwner, wordsView::setWords)
         vm.loading.observe(viewLifecycleOwner, wordsView::setLoading)
     }
--- app/src/main/java/com/example/yaroslavgorbach/voclevelup/screen/words/WordsListAdapter.kt
+++ app/src/main/java/com/example/yaroslavgorbach/voclevelup/screen/words/WordsListAdapter.kt
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.yaroslavgorbach.voclevelup.R
 import com.example.yaroslavgorbach.voclevelup.data.Word
+import com.example.yaroslavgorbach.voclevelup.databinding.ItemWordBinding
 
 class WordsListAdapter(
     private val onWordClick: (Word) -> Unit
@@ -16,24 +17,24 @@ class WordsListAdapter(
     override fun areContentsTheSame(oldItem: Word, newItem: Word) = oldItem == newItem
 }) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = WordVh(parent) {
-        onWordClick(getItem(it))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordVh {
+        return WordVh(ItemWordBinding.inflate(LayoutInflater.from(parent.context), parent, false)) {
+            onWordClick(getItem(it))
+        }
     }
+
 
     override fun onBindViewHolder(holder: WordVh, position: Int) = holder.bind(getItem(position))
 
-    class WordVh(parent: ViewGroup, onItemClick: (Int) -> Unit) : RecyclerView.ViewHolder(
-        LayoutInflater.from(parent.context).inflate(R.layout.item_word, parent, false)
-    ) {
-
-        private val text = itemView.findViewById<TextView>(R.id.wordText)
+    class WordVh(private val binding: ItemWordBinding, onItemClick: (Int) -> Unit) :
+        RecyclerView.ViewHolder(binding.root) {
 
         init {
-            itemView.setOnClickListener { onItemClick(adapterPosition) }
+            binding.root.setOnClickListener { onItemClick(adapterPosition) }
         }
 
         fun bind(word: Word) {
-            text.text = word.text
+            binding.wordText.text = word.text
         }
     }
 }
