b25a989feaa23f8037e07abd1456b5d280ab0f43
YaroslavGorbach-code <yaroslavgorbach2@gmail.com>
2021-04-10 11:53:13 +0300

    Refactor translation feature

--- app/src/main/java/com/example/yaroslavgorbach/voclevelup/screen/addword/AddWordWragment.kt
+++ app/src/main/java/com/example/yaroslavgorbach/voclevelup/screen/addword/AddWordWragment.kt
@@ -15,7 +15,8 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
-import java.io.IOException
+import com.example.yaroslavgorbach.voclevelup.feature.TranslationFeature.State.*
+import com.example.yaroslavgorbach.voclevelup.feature.TranslationFeature
 
 class AddWordFragment : Fragment(R.layout.fragment_add_word) {
     interface Host {
@@ -42,43 +43,26 @@ class AddWordFragment : Fragment(R.layout.fragment_add_word) {
         }
 
         // init translation
+        val trans = TranslationFeature(repo)
         val transTv = view.findViewById<TextView>(R.id.addWordTranslation)
-        lifecycleScope.launchWhenStarted {
-            inputEt.input(lifecycleScope).consumeAsFlow()
-                .map { it.trim() }
-                .distinctUntilChanged()
-                .debounce(400)
-                .flatMapLatest {
-                    if (it.length > 1) {
-                        flow {
-                            emit(TranslationResult.Progress)
-                            val trans = try {
-                                repo.getTranslation(it)
-                            } catch (e: IOException) {
-                                null
-                            }
-                            emit(if (trans != null) TranslationResult.Success(trans) else TranslationResult.Fail)
-                        }
-                    } else {
-                        flowOf(TranslationResult.Empty)
-                    }
-                }
-                .collect {
-                    transTv.isEnabled = it is TranslationResult.Success
-                    when (it) {
-                        TranslationResult.Empty -> transTv.text = ""
-                        TranslationResult.Progress -> transTv.setText(R.string.loading_translation)
-                        TranslationResult.Fail -> transTv.setText(R.string.cant_load_translation)
-                        is TranslationResult.Success -> transTv.text = it.result
-                    }
-                }
-        }
-    }
+        val transFlow = inputEt.input(lifecycleScope).consumeAsFlow()
+            .map { it.trim() }
+            .distinctUntilChanged()
+            .debounce(400)
+            .flatMapLatest {
+                if (it.length > 1) trans.getTranslation(it) else flowOf(null)
+            }
 
-    private sealed class TranslationResult {
-        object Empty : TranslationResult()
-        object Progress : TranslationResult()
-        object Fail : TranslationResult()
-        data class Success(val result: String) : TranslationResult()
+        lifecycleScope.launchWhenStarted {
+            transFlow.collect {
+                transTv.isEnabled = it is Success
+                when (it) {
+                    null -> transTv.text = ""
+                    Progress -> transTv.setText(R.string.loading_translation)
+                    Fail -> transTv.setText(R.string.cant_load_translation)
+                    is Success -> transTv.text = it.result
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/yaroslavgorbach/voclevelup/screen/word/WordFragment.kt
+++ app/src/main/java/com/example/yaroslavgorbach/voclevelup/screen/word/WordFragment.kt
@@ -9,35 +9,39 @@ import androidx.lifecycle.lifecycleScope
 import com.example.yaroslavgorbach.voclevelup.R
 import com.example.yaroslavgorbach.voclevelup.data.Word
 import com.example.yaroslavgorbach.voclevelup.data.repo
-import java.io.IOException
-
+import com.example.yaroslavgorbach.voclevelup.util.clicks
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.*
+import com.example.yaroslavgorbach.voclevelup.feature.TranslationFeature.State.*
+import com.example.yaroslavgorbach.voclevelup.feature.TranslationFeature
 class WordFragment : Fragment(R.layout.fragment_word) {
 
     companion object Args {
-        fun argsOf(word: Word) = bundleOf("word_text" to word.text)
-        private val WordFragment.wordText get() = requireArguments()["word_text"] as String
+        fun argsOf(word: Word) = bundleOf("word" to word.text)
+        private val WordFragment.wordText get() = requireArguments()["word"] as String
     }
 
 
+    @ExperimentalCoroutinesApi
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         // init word text
         view.findViewById<TextView>(R.id.wordText).text = wordText
 
         // init translation
         val transTv = view.findViewById<TextView>(R.id.wordTranslation)
-        val loadTranslation = {
-            transTv.isEnabled = false
-            transTv.setText(R.string.loading_translation)
-            lifecycleScope.launchWhenStarted {
-                try {
-                    transTv.text = repo.getTranslation(wordText)
-                } catch (e: IOException) {
-                    transTv.setText(R.string.reload_translation)
+        val trans = TranslationFeature(repo)
+        val transFlow = transTv.clicks(lifecycleScope).consumeAsFlow()
+            .onStart { emit(Unit) } // immediately load translation
+            .flatMapLatest { trans.getTranslation(wordText) }
+
+        lifecycleScope.launchWhenStarted {
+            transFlow.collect {
+                when(it){
+                    Fail -> transTv.setText(R.string.reload_translation)
+                    Progress -> transTv.setText(R.string.loading_translation)
+                    is Success -> transTv.text = it.result
                 }
-                transTv.isEnabled = true
             }
         }
-        transTv.setOnClickListener { loadTranslation() }
-        loadTranslation() // load translation immediately
     }
 }
