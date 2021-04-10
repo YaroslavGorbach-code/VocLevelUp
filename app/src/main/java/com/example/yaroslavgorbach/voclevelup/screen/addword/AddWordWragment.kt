81a49d86ac7089a9b355961c5ee32fc790bbdcf2
YaroslavGorbach-code <yaroslavgorbach2@gmail.com>
2021-04-10 11:06:13 +0300

    Disable save button if word length is 1 char

--- app/src/main/java/com/example/yaroslavgorbach/voclevelup/screen/addword/AddWordWragment.kt
+++ app/src/main/java/com/example/yaroslavgorbach/voclevelup/screen/addword/AddWordWragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
+import androidx.core.text.trimmedLength
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
@@ -29,7 +30,7 @@ class AddWordFragment : Fragment(R.layout.fragment_add_word) {
         // init save button
         val saveBtn = view.findViewById<View>(R.id.addWordSave)
         inputEt.doAfterTextChanged {
-            saveBtn.isEnabled = it?.isNotBlank() == true
+            saveBtn.isEnabled = it != null && it.trimmedLength() > 1
         }
 
         saveBtn.setOnClickListener {
